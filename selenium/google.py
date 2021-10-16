9a112bc2e50dad8f4e0fcdb10df676e2e7c37661
guk4979 <guk5871@gmail.com>
2021-10-17 03:41:15 +0900

    날것의 크롤링 프로그램

---
+++ selenium/google.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+keyWords = "박보영"         #키워드 입력
+limit = 10                  #사진 개수 제한
+
+driver = webdriver.Chrome(executable_path="selenium\chromedriver.exe")
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys(keyWords)
+elem.send_keys(Keys.RETURN)
+
+# SCROLL_PAUSE_TIME = 1
+# # Get scroll height
+# last_height = driver.execute_script("return document.body.scrollHeight")
+# while True:
+#     # Scroll down to bottom
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     # Wait to load page
+#     time.sleep(SCROLL_PAUSE_TIME)
+#     # Calculate new scroll height and compare with last scroll height
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         try:
+#             driver.find_element_by_css_selector(".mye4qd").click()
+#         except:
+#             break
+#     last_height = new_height
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 0
+for image in images:
+    if count == limit:
+        break
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl,keyWords + str(count+1) + ".jpg")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
